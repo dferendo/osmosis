d6eb1294c852f5577f6288856ab0cfd0a471b237
dferendo <dylanfrendo@gmail.com>
2022-04-06 16:38:14 +0100

    Actually fixed address for alice

--- test_pool.sh
+++ test_pool.sh
@@ -5,7 +5,7 @@ NODE_DATA="./data/$CHAIN_ID/"
 # Run the commands one-by-one in a bash
 starport chain serve --config=config.yml --reset-once
 
-osmosisd query bank balances osmo16xlwut5jjrmxape27lys2ga3jxxqhcs7qwtsea --node="$NODE" --chain-id="$CHAIN_ID" --home="$NODE_DATA"
+osmosisd query bank balances osmo1rgy3fw5z64mn8k45xpg9vxk9d90e9lj68lmq2u --node="$NODE" --chain-id="$CHAIN_ID" --home="$NODE_DATA"
 
 # Create pools
 osmosisd tx gamm create-pool --pool-file="./pools/pool_uband.json" --fees="350stake" --from=alice --node="$NODE" --chain-id="$CHAIN_ID" --home="$NODE_DATA"
